ae7954ff904040afae010cec3a01dd2b4521253d
Seyon Rajagopal <r_seyon@live.ca>
2020-12-22 17:34:32 -0500

    Added bus stops and bus route on map

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
-import ReactMapGL, {Marker, NavigationControl,ScaleControl, FullscreenControl, Layer} from "react-map-gl";
+import ReactMapGL, { Marker, Popup, NavigationControl, ScaleControl, FullscreenControl, Source, Layer } from "react-map-gl";
 import "./App.css";
+import PolylineOverlay from './PolyLineOverlay'
 
-import {fetchBusLocation} from './api'
+import { fetchBusLocation, fetchRouteStops, fetchStopTimes, fetchRoutePath } from "./api";
 
 function App() {
+	const busRoute = 35;
 
 	const [viewport, setviewport] = useState({
 		latitude: 43.6534817,
@@ -12,75 +14,190 @@ function App() {
 		width: "100vw",
 		height: "100vh",
 		zoom: 10,
-		bearing: -17
+		bearing: -17,
 	});
 
 	const [busLocation, setBusLocation] = useState([]);
+	const [routePath, setRoutePath] = useState([]);
+	const [routeStops, setRouteStops] = useState([]);
+	const [selectedStop, setSelectedStop] = useState(null);
+	const [stopTimes, setStopTimes] = useState({});
 
 	useEffect(() => {
 		const fetchBusAPI = async () => {
-            setBusLocation(await fetchBusLocation(36));
+			setBusLocation(await fetchBusLocation(busRoute));
+			setRoutePath(await fetchRoutePath(busRoute))
+			setRouteStops(await fetchRouteStops(busRoute));	
 		};
-		fetchBusAPI()
-		const interval = setInterval(() => fetchBusAPI(), 10000)
-        return () => {
-          clearInterval(interval);
-        }
-	}, [])
 
-	console.log(busLocation);
+		fetchBusAPI();
+		const interval = setInterval(() => fetchBusAPI(), 5000);
+		return () => {
+			clearInterval(interval);
+		};
+	}, []);
+
+	useEffect(() => {
+		console.log("RoutePath", routePath);
+	}, [busLocation])
+
+	useEffect(() => {
+		const fetchAPI = async () => {
+			setStopTimes(await fetchStopTimes(busRoute, selectedStop?.stopId));
+		};
+		fetchAPI();
+		const interval = setInterval(() => fetchAPI(), 5000);
+		return () => {
+			clearInterval(interval);
+		};
+		
+	}, [selectedStop]);
+
+	useEffect(() => {
+		const listener = (e) => {
+			if (e.key === "Escape") {
+				setSelectedStop(null);
+			}
+		};
+		window.addEventListener("keydown", listener);
+
+		return () => {
+			window.removeEventListener("keydown", listener);
+		};
+	}, []);
 
 	return (
 		<div className="App">
-			<ReactMapGL {...viewport} 
+
+			<ReactMapGL
+				{...viewport}
 				mapboxApiAccessToken="pk.eyJ1Ijoic2V5b24xMDAiLCJhIjoiY2tpejhqcTh5MWE4djJ5cDNpaHUxMzBvcyJ9.7yaG59iuW_HA0xr4wY6btQ"
 				mapStyle="mapbox://styles/seyon100/ckiz9arnc0sxe19o51hatbmuv"
-				onViewportChange={ viewport => {
-					if(viewport.zoom < 10) {
+				containerStyle={{ flex: 1 }}
+				onViewportChange={(viewport) => {
+					if (viewport.zoom < 10) {
 						viewport.zoom = 10;
+					} else if (viewport.zoom > 18) {
+						viewport.zoom = 18;
 					}
+
 					setviewport(viewport);
 				}}
 			>
-				{busLocation?.length > 0 ?
-					busLocation.map(({id,lat,lon}) => (
-						<Marker key={id} latitude={Number(lat)} longitude={Number(lon)}>
-							<img className="busMarker" src="bus.png"/>
-						</Marker>
+				{/* Showpath og route */}
+				{routePath.length > 0 ?
+					routePath.map(({point}) => (
+						<PolylineOverlay points={point.map((p) => ([Number(p.lon), Number(p.lat)]))}/>
 					))
-				:""}
-				<div style={{position: 'absolute', right: 10, bottom: 70}}>
+				: null
+				}
+
+				{/* Show Stops */}
+				{routeStops?.length > 0
+					? routeStops.map(({ stopId, lat, lon, title }) => (
+							<Marker key={stopId} latitude={Number(lat)} longitude={Number(lon)} offsetLeft={-10} offsetTop={-10}>
+								<button
+									className="busStopBtn"
+									onClick={(e) => {
+										e.preventDefault();
+										setSelectedStop({ stopId, title, lat, lon });
+									}}
+								>
+									<div className="busStopIcon" />
+								</button>
+							</Marker>
+					  ))
+					: ""}
+
+				{/* Show Bus Location */}
+				{busLocation?.length > 0
+					? busLocation.map(({ id, lat, lon }) => (
+							<Marker key={id} latitude={Number(lat)} longitude={Number(lon)} offsetLeft={-20} offsetTop={-20}>
+								<img className="busMarker" src="bus.png" />
+							</Marker>
+					  ))
+					: ""}
+
+				{/* Show Bus Time info when bus stop is clicked */}
+				{selectedStop ? (
+					<Popup
+						latitude={Number(selectedStop.lat)}
+						longitude={Number(selectedStop.lon)}
+						onClose={() => {
+							setSelectedStop(null);
+							setStopTimes({});
+						}}
+					>
+						<div>
+							<h2>{selectedStop.title}</h2>
+							{stopTimes?.length > 0 ? (
+								stopTimes.map((route) => (
+									<>
+										<p>
+											<strong>{route?.title}</strong>
+										</p>
+										{route.prediction?.length > 0
+											? route.prediction.map((bus) => (
+													<>
+														<p>{`in ${bus.minutes} minutes`}</p>
+													</>
+											  ))
+											: ""}
+									</>
+								))
+							) : (
+								<>
+									<p>
+										<strong>{stopTimes?.title}</strong>
+									</p>
+									{stopTimes?.prediction?.length > 0
+										? stopTimes.prediction.map((bus) => (
+												<>
+													{console.log(bus)}
+													<p>{`in ${bus.minutes} minutes`}</p>
+												</>
+										  ))
+										: ""}
+								</>
+							)}
+						</div>
+					</Popup>
+				) : (
+					""
+				)}
+
+				{/* Map Navigation Buttons */}
+				<div style={{ position: "absolute", right: 10, bottom: 70 }}>
 					<NavigationControl />
 				</div>
-				<div style={{position: 'absolute', right: 10, bottom: 30}}>
-					<FullscreenControl/>
+				<div style={{ position: "absolute", right: 10, bottom: 30 }}>
+					<FullscreenControl />
 				</div>
-				<div style={{position: 'absolute', right: 60, bottom: 30}}>
-					<ScaleControl maxWidth={100} unit={"metric"}/>
+				<div style={{ position: "absolute", right: 60, bottom: 30 }}>
+					<ScaleControl maxWidth={100} unit={"metric"} />
 				</div>
-						
 				
+{/* 
 				<Layer
 					id="3d-buildings"
 					source="composite"
 					source-layer="building"
-					filter={['==', 'extrude', 'true']}
+					filter={["==", "extrude", "true"]}
 					type="fill-extrusion"
 					minZoom={6}
 					paint={{
-						'fill-extrusion-color': '#aaa',
-						'fill-extrusion-height': {
-						  type: 'identity',
-						  property: 'height'
+						"fill-extrusion-color": "#aaa",
+						"fill-extrusion-height": {
+							type: "identity",
+							property: "height",
 						},
-						'fill-extrusion-base': {
-						  type: 'identity',
-						  property: 'min_height'
+						"fill-extrusion-base": {
+							type: "identity",
+							property: "min_height",
 						},
-						'fill-extrusion-opacity': 0.6
-					  }}
-				/>
-
+						"fill-extrusion-opacity": 0.6,
+					}}
+				/> */}
 			</ReactMapGL>
 		</div>
 	);
--- src/api/index.js
+++ src/api/index.js
@@ -5,15 +5,81 @@ const agency = "ttc"
 
 
 export const fetchBusLocation = async (route) => {
-    
     let url = `${api}command=vehicleLocations&a=${agency}&r=${route}`;
-    
     try {
         const{
             data: {vehicle},
         } = await axios.get(url);
-        console.log(vehicle)
+        // console.log(vehicle)
         return vehicle
+        
+    } catch (error) {
+        return error;
+    }
+};
+
+export const fetchRouteStops = async (route) => {
+    let url = `${api}command=routeConfig&a=${agency}&r=${route}`;
+    try {
+        const{
+            data: {route: {stop}},
+        } = await axios.get(url);
+        // console.log(stop)
+
+        return stop
+        
+    } catch (error) {
+        return error;
+    }
+};
+
+export const fetchStopTimes = async (route, stopId) => {
+    let url = `${api}command=predictions&a=${agency}&r=${route}&stopId=${stopId}`;
+    try {
+        const{
+            data: {predictions: {direction}},
+        } = await axios.get(url);
+        // console.log(url)
+        // console.log(direction)
+
+        return direction
+        
+    } catch (error) {
+        return error;
+    }
+};
+
+
+function sortFunction(a, b) {
+    if (a[0] === b[0]) {
+        return 0;
+    }
+    else {
+        return (a[0] < b[0]) ? -1 : 1;
+    }
+}
+// WIP
+export const fetchRoutePath = async (routeNum) => {
+ 
+    let url = `${api}command=routeConfig&a=${agency}&r=${routeNum}`;
+    try {
+        const{
+            data: {route},
+        } = await axios.get(url);
+
+        // let routepath = [];
+
+        // if(route.path?.length > 0){
+        //     route.path.map(({point}) => (
+        //         point.map((p) => (
+        //             routepath.push([Number(p.lon), Number(p.lat)])
+        //         ))
+        //     ))
+        // }
+        console.log(url)
+        console.log(route.path);
+
+        return route.path
         
     } catch (error) {
         return error;
